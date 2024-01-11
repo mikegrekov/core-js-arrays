7143943d7124c310c0f364a309ae69878fefb123
mikegrekov <svg200914@gmail.com>
2024-01-11 00:41:13 +0000

    deploy

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -411,8 +411,12 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((acc, val) => {
+    let num = acc;
+    if (!(val || false)) num += 1;
+    return num;
+  }, 0);
 }
 
 /**
